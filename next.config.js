cd262d029a96d47a3cec5badddf0d7fc3ed473d7
Eren BALCI <erenbalci3939@gmail.com>
2020-12-24 00:03:16 +0300

    fixed

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     MONGO_URI: process.env.MONGO_URI,
     SECRET_KEY: process.env.SECRET_KEY,
-    WEB_URL: `${process.env.WEB_URL}:${process.env.PORT}`,
+    WEB_URL: "http://34.65.145.212:8000",
     PORT: process.env.PORT,
   },
 };
